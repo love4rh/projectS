127a42a08a647458d76f55e1ca070cdc995720ef
Kim, MyungHyun <mh9.kim@lge.com>
2021-06-08 16:58:30 +0900

    temporary works at 2021-06-08

--- config.py
+++ config.py
@@ -2,16 +2,21 @@
 
 # config.py
 
-# 기본 폴더 위치
-dirBase = 'D:\\workspace\\krx\\'
+# 네이버 API를 호출하기 위한 암호화 파라미터
+# 만료 기간이 지나면 네이버에서 확인 후 수정해야 함.
+encParamKey = 'MzcxaGJUeEt4blBJQmVmT1hiWnRHUT09'
 
-# 조회할 기업 코드 목록 파일
-# codePathFile = '/home/ec2-user/krx/codes.txt'
-codePathFile = dirBase + 'codes.txt'
+onLinux = False
+
+# 기본 폴더 위치
+dirBase = 'D:\\work\\jupyter\\krx\\' # for Window
+resourceDir = dirBase + 'resource\\'
 
 # 재무제표 크롤링 데이터 저장 위치
-# outputRawPath = '/home/ec2-user/krx/raw/'
-outputRawPath = 'D:\\workspace\\krx\\temporary\\'
+outputRawPath = dirBase + 'output\\'
+
+# 임시파일 저장용 폴더
+temporaryPath = dirBase + 'temporary\\'
 
 # Crawlego 경로
 crawlegoPath = '/home/ec2-user/krx/analysis/jar/crawlego-1.0.0.jar'
@@ -19,5 +24,13 @@ crawlegoPath = '/home/ec2-user/krx/analysis/jar/crawlego-1.0.0.jar'
 # Pivot용 Jar 파일 경로
 pivotPath = '/home/ec2-user/krx/analysis/jar/pivot.jar'
 
+if onLinux:
+    dirBase = '/home/ec2-user/krx/'
+    resourceDir = dirBase + 'resource/'
+    outputRawPath = dirBase + 'output/'
+    temporaryPath = dirBase + 'temporary/'
+    crawlegoPath = '/home/ec2-user/krx/analysis/jar/crawlego-1.0.0.jar'
+    pivotPath = '/home/ec2-user/krx/analysis/jar/pivot.jar'
 
-
+# 조회할 기업 코드 목록 파일
+codePathFile = resourceDir + 'companyInfo-20210530.txt'
